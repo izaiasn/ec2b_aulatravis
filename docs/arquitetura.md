810bc41c3ecff148a02e90d4416ca593954c13e8
Izaias Nilson de Souza <you@example.com>
2020-04-06 22:01:13 -0300

    travis

---
+++ docs/arquitetura.md
@@ -0,0 +1,2 @@
+#arquivo de arquitetura 
+
---
+++ src/teste.py
@@ -0,0 +1,21 @@
+import jogo_da_velha
+import sys
+
+erroInicializar = False
+jogo = jogo_da_velha.inicializar()
+
+if len(jogo) != 3:
+    erroInicializar = True
+else:
+    for linha in jogo:
+        if len(linha) != 3:
+            erroInicializar = True
+        else:
+            for elemento in linha:
+                if elemento != '.':
+                    erroInicializar = True
+
+if erroInicializar:
+    sys.exit(1)
+else:
+    sys.exit(0)
